d119b53f342d4e04ae30fb8a248e541abba6c615
Arsenii Misiurenko <misiurenko.arsenii@gmail.com>
2025-10-07 23:55:05 +0300

    Basic api

---
+++ app/api/is-even/[number]/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest } from 'next/server';
+ 
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ number: number }> },
+) {
+  const checkNumber = (await params).number;
+
+  return new Response(JSON.stringify({ isEven: checkNumber % 2 == 0 }), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
