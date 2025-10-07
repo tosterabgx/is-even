d1d1c401b84d120b358d9ae73699ff3780c84cd8
Arsenii Misiurenko <misiurenko.arsenii@gmail.com>
2025-10-07 23:42:52 +0300

    Main page update (no frontend yet)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
-  return;
+  const [result, setResult] = useState("");
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setResult(+e.target.value % 2 == 0 ? "Even" : "Odd");
+  }
+
+  return (
+    <>
+      {result}
+      <br />
+      <input
+        className="border border-black p-4"
+        type="number"
+        onChange={handleInput}
+      />
+    </>
+  );
 }
